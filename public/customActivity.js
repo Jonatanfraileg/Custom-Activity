6731741f70d4588b74be6fd148cf1920a0dfffa1
jfraileg <jfraileg@everis.com>
2017-07-20 10:46:40 +0200

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -59,7 +59,7 @@ define(function (require) {
 		payload['arguments'] = payload['arguments'] || {};
 		payload['arguments'].execute = payload['arguments'].execute || {};
 		payload['arguments'].execute.inArguments = [{
-			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"<EVENT DATA ID PATH>\"}}'
+			'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"Everis2017__SEAT_Responses__c:Everis2017__SEAT_Survey__c\"}}'
 		}];
 
 		payload['metaData'] = payload['metaData'] || {};
